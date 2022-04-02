4d21e34927b85d73a50f22b1021ad4e673a75bf5
Watts-Blake <watts.blake52@gmail.com>
2022-04-02 02:05:57 -0700

    added stores to front end

---
+++ react-app/src/store/message.js
@@ -0,0 +1,139 @@
+import { csrfFetch } from "./csrf";
+
+//load messages
+//post message
+//edit message
+//delete message
+//--------------------------------------------load channel messages-----------------------
+const LOAD_CHANNELMESSAGES = "messages/LoadChannelMessages";
+export const loadChannelMessages = (messages) => {
+  return { type: LOAD_CHANNELMESSAGES, messages };
+};
+
+export const getChannelMessages = (channelId) => async (dispatch) => {
+  const res = await fetch(`/api/messages/channels/${channelId}`);
+  const getMessages = await res.json();
+  dispatch(loadChannelMessages(getMessages));
+};
+
+//--------------------------------------------load direct messages-----------------------
+const LOAD_DIRECTMESSAGES = "messages/LoadDirectMessages";
+export const loadDirectMessages = (messages) => {
+  return { type: LOAD_DIRECTMESSAGES, messages };
+};
+
+export const getDirectMessages = (dmRoomId) => async (dispatch) => {
+  const res = await fetch(`/api/messages/dm_rooms/${dmRoomId}`);
+  const getMessages = await res.json();
+  dispatch(loadDirectMessages(getMessages));
+};
+
+//--------------------------------------------add channel message-----------------------
+
+const ADD_CHANNELMESSAGE = "messages/AddChannelMessage";
+export const addChannelMessage = (message) => ({
+  type: ADD_CHANNELMESSAGE,
+  message,
+});
+
+export const postChannelMessage = (message) => async (dispatch) => {
+  const res = await csrfFetch(`/api/messages/channels/${message.channel_id}`, {
+    method: "POST",
+    body: JSON.stringify(message),
+  });
+  const newMessage = await res.json();
+
+  dispatch(addChannelMessage(newMessage));
+};
+
+//--------------------------------------------add direct message-----------------------
+
+const ADD_DIRECTMESSAGE = "messages/AddDirectMessage";
+export const addDirectMessage = (message) => ({
+  type: ADD_DIRECTMESSAGE,
+  message,
+});
+
+export const postDirectMessage = (message) => async (dispatch) => {
+  const res = await csrfFetch(`/api/messages/dm_rooms/${message.dm_room_id}`, {
+    method: "POST",
+    body: JSON.stringify(message),
+  });
+  const newMessage = await res.json();
+
+  dispatch(addDirectMessage(newMessage));
+};
+
+//--------------------------------------------update message-----------------------
+const UPDATE_MESSAGE = "messages/UpdateMessage";
+export const updateMessage = (message) => ({
+  type: UPDATE_MESSAGE,
+  message,
+});
+export const putMessage = (message) => async (dispatch) => {
+  const res = await csrfFetch(`/api/messages/${message.id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(message),
+  });
+  const updatedMessage = await res.json();
+  dispatch(updateMessage(updatedMessage));
+};
+
+//--------------------------------------------delete message-----------------------
+const DELETE_MESSAGE = "messages/deleteMessage";
+export const removeMessage = (messageId) => ({
+  type: DELETE_MESSAGE,
+  messageId,
+});
+export const deleteMessage = (messageId) => async (dispatch) => {
+  const res = await csrfFetch(`/api/messages/${messageId}`, {
+    method: "DELETE",
+  });
+
+  const deletedMessage = await res.json();
+
+  dispatch(removeMessage(deletedMessage));
+};
+//---------------------------------------------reducer----------------------------
+
+const messageReducer = (state = {}, action) => {
+  let newState = { ...state };
+
+  switch (action.type) {
+    case LOAD_CHANNELMESSAGES: {
+      action.message.forEach((message) => {
+        return (newState[message.id] = message);
+      });
+      return newState;
+    }
+    case LOAD_DIRECTMESSAGES: {
+      action.message.forEach((message) => {
+        return (newState[message.id] = message);
+      });
+      return newState;
+    }
+    case ADD_CHANNELMESSAGE: {
+      newState[action.message.id] = { ...action.message };
+
+      return newState;
+    }
+    case ADD_DIRECTMESSAGE: {
+      newState[action.message.id] = { ...action.message };
+
+      return newState;
+    }
+    case UPDATE_MESSAGE: {
+      newState[action.message.id] = action.message;
+      return newState;
+    }
+    case DELETE_MESSAGE: {
+      delete newState[action.messageId];
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+
+export default messageReducer;
---
+++ react-app/src/store/workspace.js
@@ -0,0 +1,107 @@
+import { csrfFetch } from "./csrf";
+//--------------------------------------------load workspaces-----------------------
+const LOAD_WORKSPACES = "workspaces/LoadWorkspaces";
+export const loadWorkspaces = (workspaces) => {
+  return { type: LOAD_WORKSPACES, workspaces };
+};
+
+export const getWorkspaces = (userId) => async (dispatch) => {
+  const res = await fetch(`/api/workspaces/${userId}`);
+  const workspaces = await res.json();
+  dispatch(loadWorkspaces(workspaces));
+};
+//-------------------------------------------get one workspace------------------------
+const LOAD_ONEWORKSPACE = "workspaces/LoadOneWorkspace";
+export const loadOneWorkspace = (workspace) => {
+  return { type: LOAD_ONEWORKSPACE, workspace };
+};
+
+export const getOneWorkspace = (workspaceId) => async (dispatch) => {
+  const res = await fetch(`/api/workspaces/${workspaceId}`);
+  const workspace = await res.json();
+  dispatch(loadOneWorkspace(workspace));
+};
+
+//--------------------------------------------add workspace-----------------------
+
+const ADD_WORKSPACE = "workspaces/AddWorkspace";
+export const addWorkspace = (workspace) => ({
+  type: ADD_WORKSPACE,
+  workspace,
+});
+
+export const postWorkspace = (workspace) => async (dispatch) => {
+  const res = await csrfFetch("/api/workspaces", {
+    method: "POST",
+    body: JSON.stringify(workspace),
+  });
+  const workspace = await res.json();
+
+  dispatch(addWorkspace(workspace));
+};
+//--------------------------------------------update workspace-----------------------
+const UPDATE_WORKSPACE = "workspaces/UpdateWorkspace";
+export const updateWorkspace = (workspace) => ({
+  type: UPDATE_WORKSPACE,
+  workspace,
+});
+export const putWorkspace = (workspace) => async (dispatch) => {
+  const res = await csrfFetch(`/api/workspaces/${workspace.id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(workspace),
+  });
+  const updatedWorkspace = await res.json();
+  dispatch(updateWorkspace(updatedWorkspace));
+};
+
+//--------------------------------------------delete workspace-----------------------
+const DELETE_WORKSPACE = "workspaces/deleteWorkspace";
+export const removeEvent = (workspaceId) => ({
+  type: DELETE_WORKSPACE,
+  workspaceId,
+});
+export const deleteEvent = (workspaceId) => async (dispatch) => {
+  const res = await csrfFetch(`/api/workspaces/${workspaceId}`, {
+    method: "DELETE",
+  });
+
+  const workspaceNum = await res.json();
+
+  dispatch(removeEvent(workspaceNum));
+};
+//---------------------------------------------reducer----------------------------
+
+const workspaceReducer = (state = { currentWorkspace: {} }, action) => {
+  let newState = { ...state };
+
+  switch (action.type) {
+    case LOAD_WORKSPACES: {
+      action.workspaces.forEach((workspace) => {
+        return (newState[workspace.id] = workspace);
+      });
+      return newState;
+    }
+    case ADD_WORKSPACE: {
+      newState[action.workspace.id] = { ...action.workspace };
+
+      return newState;
+    }
+    case UPDATE_WORKSPACE: {
+      newState[action.workspace.id] = action.workspace;
+      return newState;
+    }
+    case DELETE_WORKSPACE: {
+      delete newState[action.workspaceId];
+      return newState;
+    }
+    case LOAD_ONEWORKSPACE: {
+      newState.currentWorkspace = action.workspace;
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+
+export default workspaceReducer;
