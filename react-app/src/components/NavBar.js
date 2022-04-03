a969d4638b073ca0eec31ba5b9e4fec2fd06fa5e
Johnny San <johnny.san@gmail.com>
2022-04-03 12:13:43 -0700

    added profile pic to right side of nav

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -12,22 +12,23 @@ const NavBar = () => {
   if (user) {
     return (
       <nav className='nav-bar'>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
+        <ul>
+          <li>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/users' exact={true} activeClassName='active'>
+              Users
+            </NavLink>
+          </li>
+          <li>
+            <LogoutButton />
+          </li>
+        </ul>
+        {user.profile_picture ? <img src={user.profile_picture} alt='profile' style={{ height: 25, marginRight:5 }} /> : <div style={{marginRight:5}}>{user.username}</div>}
+      </nav>
     )
   }
   return (
