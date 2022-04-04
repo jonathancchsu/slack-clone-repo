40a7a07e3dec6133323c557f38cb30697ae27c47
Watts-Blake <watts.blake52@gmail.com>
2022-04-03 22:21:23 -0700

    displayed correct content for main content page

--- react-app/src/components/OneWorkspace/MainContent.js/index.js
+++ react-app/src/components/OneWorkspace/MainContent.js/index.js
@@ -3,25 +3,44 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { getChannelMessages, getDirectMessages } from "../../../store/message";
+
 const MainContent = () => {
   const dispatch = useDispatch();
   const [loaded, setloaded] = useState(false);
-  let { id } = useParams();
+
   let url = window.location.href;
-  console.log(url.includes("dm_rooms"));
+  const [dmRoom, setDmRoom] = useState(url.includes("dm_rooms"));
+  const [channelRoom, setChannelRoom] = useState(url.includes("channels"));
+
+  let { id } = useParams();
+
+  const messagesObj = useSelector((state) => state.message);
+  const messages = Object.entries(messagesObj);
+
+  console.log("hereeeeeeeeeeee", messages);
 
   useEffect(() => {
-    if (url.includes("channels")) {
+    if (channelRoom) {
       //dispatch the thing.then setloaded true
+      dispatch(getChannelMessages(id)).then(() => setloaded(true));
     }
-    if (url.includes("dm_rooms")) {
+    if (dmRoom) {
       //dispatch the thing.then setloaded true
+      dispatch(getDirectMessages(id)).then(() => setloaded(true));
     }
-  });
+  }, []);
   return (
-    <div>
-      <h2>Main Content</h2>
-    </div>
+    loaded && (
+      <div>
+        {messages.map((message) => (
+          <div key={message[1].id}>
+            {message[1].content} {message[1].sender_username}{" "}
+            {message[1].created_at}
+          </div>
+        ))}
+      </div>
+    )
   );
 };
 
