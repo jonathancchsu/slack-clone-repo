f1b0c1d128b5461a9073095602fe4871e3f88d7d
carlos rodriguez <twincarlos98@gmail.com>
2022-04-04 11:30:13 -0400

    added socket-io implementation

--- react-app/src/components/OneWorkspace/MainContent.js/index.js
+++ react-app/src/components/OneWorkspace/MainContent.js/index.js
@@ -2,8 +2,10 @@ import "./MainContent.css";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { getChannelMessages, getDirectMessages } from "../../../store/message";
+// import { getChannelMessages, getDirectMessages } from "../../../store/message";
 import { getCurrentChannel, getCurrentRoom } from "../../../store/workspace";
+import { io } from 'socket.io-client';
+let socket;
 
 const MainContent = () => {
   const [loaded, setloaded] = useState(false);
@@ -11,6 +13,9 @@ const MainContent = () => {
   let url = window.location.href;
   const [dmRoom, setDmRoom] = useState();
   const [channelRoom, setChannelRoom] = useState();
+  const [chatInput, setChatInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const user = useSelector(state => state.session.user);
 
   const view = useSelector((state) => state.workspace.currentView);
   useEffect(() => {
@@ -24,7 +29,27 @@ const MainContent = () => {
       setDmRoom(true);
       dispatch(getCurrentRoom(id)).then(() => setloaded(true));
     }
+
+    socket = io();
+
+    socket.on("chat", (chat) => {
+      setMessages(messages => [...messages, chat])
+    })
+    // when component unmounts, disconnect
+    return (() => {
+      socket.disconnect()
+    })
   }, [dispatch, url]);
+
+  const updateChatInput = (e) => {
+    setChatInput(e.target.value)
+  };
+
+  const sendChat = (e) => {
+    e.preventDefault()
+    socket.emit("chat", { user: user.username, msg: chatInput });
+    setChatInput("")
+  }
 
   return (
     loaded && (
@@ -42,6 +67,18 @@ const MainContent = () => {
             {message.created_at}
           </div>
         ))}
+        <div>
+          {messages.map((message, ind) => (
+            <div key={ind}>{`${message.user}: ${message.msg}`}</div>
+          ))}
+        </div>
+        <form onSubmit={sendChat}>
+          <input
+            value={chatInput}
+            onChange={updateChatInput}
+          />
+          <button type="submit">Send</button>
+        </form>
       </div>
     )
   );
