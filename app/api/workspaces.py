fa957617d250f99c4a4efdcd17c639abde0d26a5
Jonathan H <jhsucoding@gmail.com>
2022-04-05 13:37:27 -0400

    added channels api routes

--- app/api/workspaces.py
+++ app/api/workspaces.py
@@ -1,5 +1,6 @@
 # from crypt import methods
 from flask import Blueprint, render_template, redirect, url_for, request
+from app.forms.channel_form import ChannelForm
 # from flask_login import login_required
 # from sqlalchemy.orm import Session
 from app.models import User, Workspace, WorkspaceMember, Channel, ChannelMember, Message, DirectMessageRoom, DirectMessageMember, db
@@ -26,14 +27,51 @@ def get_one_dm_group(dm_id):
 @bp.route('/channels/new', methods=['POST'])
 def channel_create():
     data = request.json
-    form 
+    form = ChannelForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        channel = Channel(
+            name=form.data['name'],
+            topic=form.data['topic'],
+            description=form.data['description'],
+            owner_id=data['owner_id'],
+            workspace_id=data['workspace_id'],
+        )
+        db.session.add(channel)
+        db.session.commit()
 
 
-@bp.route('channels/<int:channel_id>')
-def get_one_channel(channel_id):
-    channel = Channel.query.get(channel_id)
-    return channel.to_dict()
+        channelMember = ChannelMember(
+            channel_id=channel.id,
+            user_id=channel.owner_id,
+        )
+        db.session.add(channelMember)
+        db.session.commit()
 
+        return channel.to_dict()
+
+
+
+@bp.route('channels/<int:channel_id>', methods=['GET', 'PUT', 'DELETE'])
+def channel(channel_id):
+    if request.method == 'GET':
+        channel = Channel.query.get(channel_id)
+        return channel.to_dict()
+
+    if request.method == 'PUT':
+        channel = Channel.query.get(channel_id)
+        data = request.json
+        channel.name = data['name']
+        channel.topic = data['topic']
+        channel.description = data['description']
+        db.session.commit()
+        return {'channel_id': channel_id}
+
+    if request.method == 'DELETE':
+        channel = db.session.query(Channel).filter(Channel.id == channel_id).first()
+        db.session.delete(channel)
+        db.session.commit()
+        return {'channel_id': channel_id}
 
 # @bp.route('/<int:user_id>')
 # def get_all_workspaces(user_id):
