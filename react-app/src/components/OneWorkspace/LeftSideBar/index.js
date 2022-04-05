0671dfbf1f03848caf86480f433c7e0e98ee4ce9
Johnny San <johnny.san@gmail.com>
2022-04-04 22:50:29 -0700

    add member to workspace working, but with bugs, a user can be added multiple times to one workspace, and api return statement needs work

--- react-app/src/components/OneWorkspace/LeftSideBar/index.js
+++ react-app/src/components/OneWorkspace/LeftSideBar/index.js
@@ -1,12 +1,23 @@
 import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAllUsers } from "../../../store/session";
+import { addAMember } from "../../../store/workspace";
 
 const LeftSideBar = ({ workspace }) => {
   let history = useHistory();
   const dispatch = useDispatch();
   const users = useSelector(state => state.session.users)
+  const [user_id, setUserID] = useState(workspace.owner.id);
+
+
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('user_id:', user_id)
+    dispatch(addAMember(user_id, workspace.id))
+    // return history.push('/')
+  }
 
   useEffect(() => {
     dispatch(getAllUsers())
@@ -24,16 +35,17 @@ const LeftSideBar = ({ workspace }) => {
   return (
     <div>
       <h2>{workspace.name}</h2>
-      <form>
-        <select>
+        <select
+          value ={user_id}
+          onChange={(e) => setUserID(e.target.value)}
+        >
          {
            users?.map((user => (
-             <option key={user.id}>{user.username}</option>
+             <option value = {user.id} key={user.id}>{user.username}</option>
            )))
          }
         </select>
-        <button>Add User</button>
-      </form>
+        <button onClick={onSubmit}>Add User</button>
       <div>
         <h3>Channels</h3>
         {workspace.channels.map((channel) => (
