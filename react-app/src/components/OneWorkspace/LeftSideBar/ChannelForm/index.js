40065742e22ed145abb1f4f1120c9660418b6eed
Jonathan H <jhsucoding@gmail.com>
2022-04-05 15:38:36 -0400

    working on channel form and channel react

---
+++ react-app/src/components/OneWorkspace/LeftSideBar/ChannelForm/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { postChannel } from "../../../../store/channel";
+
+import "./ChannelForm.css"
+
+const ChannelForm = () => {
+  
+}
--- react-app/src/components/OneWorkspace/LeftSideBar/Channels/index.js
+++ react-app/src/components/OneWorkspace/LeftSideBar/Channels/index.js
@@ -2,16 +2,29 @@ import { useHistory } from "react-router-dom";
 
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-
+import { getUser } from "../../../../store/session";
+import { deleteChannel, putChannel } from "../../../../store/channel";
 
 import "./channels.css"
 
 
 const Channels = ({ workspace, users }) => {
+  const [loaded, setLoaded] = useState(false);
+  const [edit, setEdit] = useState('');
+  const [channelName, setChannelName] = useState('');
+  const [channelTopic, setChannelTopic] = useState('');
+  const [channelDescription, setChannelDescription] = useState('');
+  const dispatch = useDispatch();
   let history = useHistory();
+
+
   const channelMessages = async (id) => {
     history.push(`/workspaces/${workspace.id}/messages/channels/${id}`);
   };
+
+  const createChannelForm = (channel_id) => {
+    history.push(`/workspaces/${channel_id}`)
+  }
 
   return (
     <div>
