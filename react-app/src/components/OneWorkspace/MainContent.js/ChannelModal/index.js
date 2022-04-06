dc76d9c6c03dbda08fc94f81e02513e9188454cf
Jonathan H <jhsucoding@gmail.com>
2022-04-06 19:06:02 -0400

    working

--- react-app/src/components/OneWorkspace/MainContent.js/ChannelModal/index.js
+++ react-app/src/components/OneWorkspace/MainContent.js/ChannelModal/index.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { addNewChannelMember } from "../../../../store/channel";
 
 import "./ChannelModal.css";
 
 const ChannelModal = ({ setShowModal }) => {
-  const channelName = useSelector()
-  const channelTopic = useSelector()
-  const channelDescription = useSelector()
-  const channelOwner = useSelector()
-  const createdAt = useSelector()
+  const dispatch = useDispatch()
+  const { channelId } = window.location.href.split('/')[7]
+
+  const [newMember, setNewMember] = useState("");
+
+  const handleAddMember = (e, id) => {
+    e.preventDefault();
+    dispatch(addNewChannelMember(id, newMember))
+    setNewMember("");
+  }
 
   return (
       <div className="channel-modal-container">
         <h1>{channelName}</h1>
-        <span>X</span>
         <div className="sub-container">
           <b>Topic</b>
           <p>{channelTopic}</p>
@@ -25,6 +31,16 @@ const ChannelModal = ({ setShowModal }) => {
         <div className="subcontainer">
           <b>Created By</b>
           <p>{channelOwner} on {createdAt}</p>
+        </div>
+        <div className="subcontainer">
+          <form onSubmit={(e) => handleAddMember(e, channelId)}>
+            <input
+              type='text'
+              value={newMember}
+              onChange={e => setNewMember(e.target.value)}
+            ></input>
+            <button>add member</button>
+          </form>
         </div>
       </div>
   )
