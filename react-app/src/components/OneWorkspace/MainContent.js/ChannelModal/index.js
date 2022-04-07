7f564a053919f57fde60e4a59c01fa4ee552d3ac
Jonathan H <jhsucoding@gmail.com>
2022-04-07 11:55:46 -0400

    work

--- react-app/src/components/OneWorkspace/MainContent.js/ChannelModal/index.js
+++ react-app/src/components/OneWorkspace/MainContent.js/ChannelModal/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import { addNewChannelMember } from "../../../../store/channel";
 
 import "./ChannelModal.css";
