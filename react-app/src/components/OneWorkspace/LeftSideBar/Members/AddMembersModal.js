0ae70e4b6020837c93fb523c74d7b6eb0d1e53a0
Johnny San <johnny.san@gmail.com>
2022-04-07 16:12:25 -0700

    fixed filter

--- react-app/src/components/OneWorkspace/LeftSideBar/Members/AddMembersModal.js
+++ react-app/src/components/OneWorkspace/LeftSideBar/Members/AddMembersModal.js
@@ -13,7 +13,7 @@ function AddMembersModal({ workspace, members, users, setShowModal }) {
 
 
     useEffect(() => {
-        let find = users.filter(user => user.username.toLowerCase().includes(query.toLowerCase()))
+        let find = users?.filter(user => user.username.toLowerCase().includes(query.toLowerCase()))
         setResults(find);
     }, [query, users]);
 
