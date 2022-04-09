249e552c207885d4fb1bdc1801053a8c7f93005e
Johnny San <johnny.san@gmail.com>
2022-04-08 22:44:41 -0700

    fixed logout version 2

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
+import { setUserWorkspaces } from '../../store/workspace';
 import './Button.css'
 
 const LogoutButton = () => {
   const dispatch = useDispatch()
   const onLogout = async (e) => {
     await dispatch(logout());
+    dispatch(setUserWorkspaces(null));
   };
 
   return <button onClick={onLogout} className='sign-out-btn'>Sign out</button>;
